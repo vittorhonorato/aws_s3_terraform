49aeb6b8fef2863264ccab69df4ed28634f3854b
vittorhonorato <vittorhonoratoo@gmail.com>
2025-06-21 22:29:04 -0300

    organizando infra path

---
+++ infra/main.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_version = ">=1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner = "vittorhonorato"
+      managed-by = "terraform"
+    }
+  }
+}
